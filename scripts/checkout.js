65d27c5154275647be1bff8518c57ab6b280625d
Milan <milan903575@gmail.com>
2025-11-16 11:16:58 +0530

    remove HTML element

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ function renderCart() {
     });
     cartSummeryHTML +=
       `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -94,5 +94,9 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((button, index) =>
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove(container);
   });
 });
